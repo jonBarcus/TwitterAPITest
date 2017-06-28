e8632a5c8dd3132575011ed7ba744b6d74dc4390
jonBarcus <barcus.j@gmail.com>
2017-06-28 13:28:52 -0400

    removed a couple of gems.

--- main.rb
+++ main.rb
@@ -1,12 +1,9 @@
-# require 'oauth2'
 require 'httparty'
 require 'json'
 require 'pry'
-# require 'base64'
 
 class TwitterTest
   include HTTParty
-  # include OAuth2
 
   @@consumer_key = ENV["TWITTER_LOCAL_API_KEY"]
   @@consumer_secret = ENV["TWITTER_LOCAL_API_SECRET"]
