a7fa6d504e15eeca32455e6e40343baefe16f867
jonBarcus <barcus.j@gmail.com>
2017-06-27 20:18:17 -0400

    tweaking the class.  added date specification

--- main.rb
+++ main.rb
@@ -10,9 +10,37 @@ class TwitterTest
 
   @@consumer_key = ENV["TWITTER_LOCAL_API_KEY"]
   @@consumer_secret = ENV["TWITTER_LOCAL_API_SECRET"]
+  @array = []
 
-  def initialize
-    binding.pry
+  def initialize(search_topic)
+
+    # credentials = Base64.encode64("#{@@consumer_key}:#{@@consumer_secret}").gsub("\n",'')
+    # url = "https://api.twitter.com/oauth2/token"
+    # body = "grant_type=client_credentials"
+    # headers = {
+    #   "Authorization" => "Basic #{credentials}",
+    #   "Content-Type" => "application/x-www-form-urlencoded;charset=UTF-8"
+    # }
+
+    # r = HTTParty.post(url, body: body, headers: headers)
+    # bearer_token = JSON.parse(r.body)['access_token']
+    # puts "Twitter bearer token is: #{bearer_token}"
+
+    bearer_token = TwitterTest.twitterAuthentication
+    date = Time.new
+    date = date.strftime("%Y-%m-%d")
+
+    api_auth_header = {"Authorization" => "Bearer #{bearer_token}"}
+    url = "https://api.twitter.com/1.1/search/tweets.json?q=#{search_topic}%20since:#{date}"
+    @results = HTTParty.get(url, headers: api_auth_header)
+    @array = []
+    @results.each do |x|
+      @array << x
+    end
+
+  end
+
+  def TwitterTest.twitterAuthentication
 
     credentials = Base64.encode64("#{@@consumer_key}:#{@@consumer_secret}").gsub("\n",'')
     url = "https://api.twitter.com/oauth2/token"
@@ -21,17 +49,11 @@ class TwitterTest
       "Authorization" => "Basic #{credentials}",
       "Content-Type" => "application/x-www-form-urlencoded;charset=UTF-8"
     }
-    binding.pry
 
     r = HTTParty.post(url, body: body, headers: headers)
     bearer_token = JSON.parse(r.body)['access_token']
-    puts "Twitter bearer token is: #{bearer_token}"
-
-    binding.pry
-
-    api_auth_header = {"Authorization" => "Bearer #{bearer_token}"}
-    url = "https://api.twitter.com/1.1/search/tweets.json?q=nba"
-    puts HTTParty.get(url, headers: api_auth_header).body
+    #puts "Twitter bearer token is: #{bearer_token}"
+    return bearer_token
 
   end
 
